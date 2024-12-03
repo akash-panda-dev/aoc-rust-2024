d2d83ccd6388f0b1b350103a559810534479e417
Akash Panda <akashajaypanda@gmail.com>
2024-12-02 23:33:23 -0800

    Day 3

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -3,10 +3,141 @@ use std::fs::read_to_string;
 
 ///////////////////////////////////////////////////////////////////////////////
 
+// Seems like I just need to parse
+// xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+
 pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let input = read_to_string("./input/day3.txt").unwrap();
+    let sol1: i64 = part1(&input);
+    let sol2: i64 = part2(&input);
 
     (Solution::from(sol1), Solution::from(sol2))
+}
+
+fn part1(input: &str) -> i64 {
+    let mut result: i64 = 0;
+    let mut input_iter = input.chars().peekable();
+
+    while let Some(c) = input_iter.next() {
+        match c {
+            'm' => {
+                if input_iter.next() == Some('u')
+                    && input_iter.next() == Some('l')
+                    && input_iter.next() == Some('(')
+                {
+                    let mut num1 = String::new();
+                    while let Some(&c) = input_iter.peek() {
+                        if !c.is_digit(10) {
+                            break;
+                        }
+                        num1.push(input_iter.next().unwrap());
+                    }
+
+                    if input_iter.next() == Some(',') {
+                        let mut num2 = String::new();
+                        while let Some(&c) = input_iter.peek() {
+                            if !c.is_digit(10) {
+                                break;
+                            }
+                            num2.push(input_iter.next().unwrap());
+                        }
+
+                        if input_iter.next() == Some(')') {
+                            if let (Ok(n1), Ok(n2)) = (num1.parse::<i64>(), num2.parse::<i64>()) {
+                                result += n1 * n2;
+                            }
+                        }
+                    }
+                }
+            }
+            _ => continue,
+        }
+    }
+
+    result
+}
+
+#[derive(Debug, Eq, PartialEq)]
+enum MulSwitch {
+    Do,
+    Dont,
+}
+
+fn part2(input: &str) -> i64 {
+    let mut result: i64 = 0;
+    let mut input_iter = input.chars().peekable();
+    let mut mul_switch = MulSwitch::Do;
+
+    while let Some(c) = input_iter.next() {
+        match c {
+            'm' => {
+                if input_iter.next() == Some('u')
+                    && input_iter.next() == Some('l')
+                    && input_iter.next() == Some('(')
+                {
+                    let mut num1 = String::new();
+                    while let Some(&c) = input_iter.peek() {
+                        if !c.is_digit(10) {
+                            break;
+                        }
+                        num1.push(input_iter.next().unwrap());
+                    }
+
+                    if input_iter.next() == Some(',') {
+                        let mut num2 = String::new();
+                        while let Some(&c) = input_iter.peek() {
+                            if !c.is_digit(10) {
+                                break;
+                            }
+                            num2.push(input_iter.next().unwrap());
+                        }
+
+                        if input_iter.next() == Some(')') {
+                            if let (Ok(n1), Ok(n2)) = (num1.parse::<i64>(), num2.parse::<i64>()) {
+                                if mul_switch == MulSwitch::Do {
+                                    result += n1 * n2;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            'd' => {
+                if input_iter.next() == Some('o') {
+                    mul_switch = MulSwitch::Do;
+                    if input_iter.next() == Some('n')
+                        && input_iter.next() == Some('\'')
+                        && input_iter.next() == Some('t')
+                    {
+                        mul_switch = MulSwitch::Dont;
+                    }
+                }
+            }
+            _ => continue,
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::part1;
+
+    #[test]
+    fn test_part1() {
+        let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        let result = part1(input);
+
+        assert_eq!(result, 161);
+    }
+
+    #[test]
+    fn test_take_while() {
+        let s = "123,456";
+        let mut chars = s.chars();
+        let nums: String = chars.by_ref().take_while(|c| c.is_digit(10)).collect();
+        assert_eq!(nums, "123");
+        assert_eq!(chars.next(), Some('4'));
+    }
 }
