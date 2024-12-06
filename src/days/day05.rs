0dd3c786ae17b83de5802f3a00d321f6bb4059a4
Akash Panda <akashajaypanda@gmail.com>
2024-12-05 20:53:33 -0800

    Day 5

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -1,9 +1,8 @@
 use crate::{Solution, SolutionPair};
 use std::{
+    cmp::Ordering,
     collections::{HashMap, HashSet},
     fs::read_to_string,
-    hash::Hash,
-    str::Lines,
 };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -11,7 +10,7 @@ use std::{
 pub fn solve() -> SolutionPair {
     let input = read_to_string("./input/day5.txt").unwrap();
     let sol1: usize = part1(&input);
-    let sol2: u64 = 0;
+    let sol2: usize = part2(&input);
 
     (Solution::from(sol1), Solution::from(sol2))
 }
@@ -20,9 +19,7 @@ fn part1(input: &str) -> usize {
     let mut parts = input.split("\n\n");
     let rules_lines = parts.next().unwrap();
     let updates = parts.next().unwrap();
-
     let adj_map = build_adjacency_map(rules_lines);
-    dbg!(&adj_map);
 
     updates
         .lines()
@@ -31,7 +28,6 @@ fn part1(input: &str) -> usize {
                 .split(",")
                 .map(|val| val.parse::<usize>().unwrap())
                 .collect::<Vec<usize>>();
-            println!("Values: {:?}", values);
 
             for w in values.windows(2) {
                 if !adj_map.contains_key(&w[0]) || !adj_map.get(&w[0]).unwrap().contains(&w[1]) {
@@ -39,14 +35,47 @@ fn part1(input: &str) -> usize {
                 }
             }
 
-            dbg!(&values);
-            Some(values[values.len() / 2])
+            Some(values[values.len() >> 1])
         })
         .sum()
 }
 
 fn part2(input: &str) -> usize {
-    todo!()
+    let mut parts = input.split("\n\n");
+    let rules_lines = parts.next().unwrap();
+    let updates = parts.next().unwrap();
+    let adj_map = build_adjacency_map(rules_lines);
+
+    updates
+        .lines()
+        .filter_map(|line| {
+            let mut values = line
+                .split(",")
+                .map(|val| val.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>();
+            let mut ordered: bool = true;
+
+            for w in values.windows(2) {
+                if !adj_map.contains_key(&w[0]) || !adj_map.get(&w[0]).unwrap().contains(&w[1]) {
+                    ordered = false;
+                }
+            }
+
+            if !ordered {
+                values.sort_by(|a, b| {
+                    if adj_map.contains_key(&a) && adj_map.get(&a).unwrap().contains(&b) {
+                        Ordering::Less
+                    } else {
+                        Ordering::Greater
+                    }
+                });
+
+                Some(values[values.len() >> 1])
+            } else {
+                None
+            }
+        })
+        .sum()
 }
 
 fn build_adjacency_map(rules_lines: &str) -> HashMap<usize, HashSet<usize>> {
